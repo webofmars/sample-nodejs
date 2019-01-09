c4e6bc38bd70d63267cd37d1fb132c09500fc20b
Frederic Leger <frederic@webofmars.com>
2019-01-09 20:56:01 +0100

    added unit tests

---
+++ test/simple.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Easy', function() {
+  describe('#simple()', function() {
+    it('should be true ... always !', function() {
+      assert.equal(true, true);
+    });
+  });
+});
